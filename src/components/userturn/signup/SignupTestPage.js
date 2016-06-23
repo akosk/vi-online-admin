df9e95f509362c74086bd3373b4cff2bff641dc1
akosk <akos.kiszely@gmail.com>
2016-06-23 10:41:10 +0200

    resolve #5 Kompetencia teszt kitöltésnél időablak kezelése

--- src/components/userturn/signup/SignupTestPage.js
+++ src/components/userturn/signup/SignupTestPage.js
@@ -1,7 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import {Panel} from'react-bootstrap';
+import {Panel,Alert} from'react-bootstrap';
 import _ from 'lodash';
+import moment from 'moment';
 
 import TestFiller from '../../test/TestFiller';
 
@@ -14,14 +15,26 @@ class SignupTestPage extends Component {
 
   render() {
     const {competency_test, user, finalized}=this.props;
+    const now = moment().valueOf();
+    const open = competency_test.start_at <= now && competency_test.end_at >= now;
     return (
       <Panel className="panel-primary" header="Kérdőív">
+        {!open &&
+        <Alert bsStyle="warning">
+          <strong> A teszt jelenleg nem elérhető.</strong>
+          <br/>
+          A teszt {moment(competency_test.start_at).format("LLL")} - {moment(competency_test.end_at).format("LLL")} időpontok között lesz elérhető.
 
+        </Alert>
+
+        }
+        {open &&
         <TestFiller
           test_id={competency_test.id}
           user={user}
           disabled={finalized ? true : false}
         />
+        }
       </Panel>
     );
   }
@@ -31,7 +44,7 @@ const mapStateToProps = (state)=>({
   user: _.get(state, 'auth.user', {}),
   currentTurn: _.get(state, 'userturns.currentTurn', {}),
   competency_test: _.get(state, 'userturns.currentTurn.competency_test', {}),
-  finalized:_.get(state,'userturns.userturn.progress.SIGNUP_COMPLETED', false)
+  finalized: _.get(state, 'userturns.userturn.progress.SIGNUP_COMPLETED', false)
 });
 
 export default connect(mapStateToProps, null)(SignupTestPage);
