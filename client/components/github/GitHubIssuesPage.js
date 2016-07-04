f578c2bdb8f8a72de9c5a0fc3aa3e946e9bb3045
akosk <akos.kiszely@gmail.com>
2016-07-04 12:58:47 +0200

    Github viewer lezárt feladatok megjelenítése

--- client/components/github/GitHubIssuesPage.js
+++ client/components/github/GitHubIssuesPage.js
@@ -4,59 +4,86 @@ import axios from 'axios';
 import _ from 'lodash';
 
 
+const itemView=(item)=>{
+  return (
+    <div>
+      <Media>
+        <Media.Left align="middle">
+          <img width={54} height={54} src={item.user.avatar_url} alt="Image" className="img-circle"/>
+        </Media.Left>
+        <Media.Body>
+          <p><strong>#{item.number}</strong> {item.title}</p>
+          <p>
+            {item.labels.map((l)=><Label style={{backgroundColor:`#${l.color}`,color:'white'}}>{l.name}</Label>)}
+          </p>
+        </Media.Body>
+      </Media>
+      <hr style={{margin:0}}/>
+    </div>
+  )
+};
+
 class GitHubIssuesPage extends Component {
 
-  constructor(props,context) {
-    super(props,context);
-    this.state={};
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
   }
 
   componentWillMount() {
     axios.get('http://api.github.com/repos/akosk/vi-online-admin/issues').then(
       (data)=> {
-        this.setState({data:data.data});
+        this.setState({ data: data.data });
+      }
+    )
+    axios.get('http://api.github.com/repos/akosk/vi-online-admin/issues?state=closed').then(
+      (data)=> {
+        this.setState({ done: data.data });
       }
     )
   }
 
   render() {
-    let list=[];
+    let list = [];
+    let list2 = [];
     if (this.state.data) {
-     list= this.state.data.map((item)=>(
-       <div>
-       <Media >
-         <Media.Left align="middle">
-           <img width={54} height={54} src={item.user.avatar_url} alt="Image" className="img-circle"/>
-         </Media.Left>
-         <Media.Body>
-           <Media.Heading>#{item.number}</Media.Heading>
-           <p>{item.title}</p>
-           <p>
-             {item.labels.map((l)=><Label style={{backgroundColor:`#${l.color}`,color:'white'}}>{l.name}</Label>)}
-           </p>
-         </Media.Body>
-       </Media>
-       <hr/>
-         </div>
-     ));
+      list = this.state.data.map(itemView);
+    }
+    if (this.state.data) {
+      list2 = this.state.done.map(itemView);
     }
     return (
-      <Panel className="panel-primary" header={(
+      <div className="row">
+        <div className="col-sm-6">
+          <Panel className="panel-primary" header={(
         <span>
         Aktuális fejlesztendő feladatok
         </span>
         )}>
 
-        <div >
-          {list}
-        </div>
+            <div >
+              {list}
+            </div>
 
-      </Panel>
+          </Panel>
+        </div>
+        <div className="col-sm-6">
+          <Panel className="panel-success" header={(
+        <span>
+        Elkészült feladatok
+        </span>
+        )}>
+
+            <div >
+              {list2}
+            </div>
+
+          </Panel>
+        </div>
+      </div>
     );
   }
 }
-
-
 
 
 export default GitHubIssuesPage;
---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Stories in Ready](https://badge.waffle.io/akosk/vi-online-admin.png?label=ready&title=Ready)](http://waffle.io/akosk/vi-online-admin)
