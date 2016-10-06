17090ad9feabbf6f0734a58144010dbe8c41450a
akosk <akos.kiszely@gmail.com>
2016-10-06 08:41:45 +0200

    resolved #103 Admin: turnus user megtekintésénél a régi kép ne jelenjen meg egy kis időre

--- client/components/turnmembers/TurnMemberView.js
+++ client/components/turnmembers/TurnMemberView.js
@@ -3,56 +3,79 @@ import {Nav, NavItem, NavDropdown} from 'react-bootstrap';
 import  {LinkContainer} from 'react-router-bootstrap';
 import Content from '../common/Content';
 import * as actions from '../../actions';
+import classnames from "classnames";
 
 import { connect } from 'react-redux';
 import _  from 'lodash';
 
 class TurnMemberView extends Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      showAvatar: false,
+      prevDisplayName: this.props.displayName
+    }
+  }
+
   componentDidMount() {
-    this.props.loadUser(this.props.params.user_id);
+    this.props.loadUser(this.props.params.user_id).then();
+  }
+
+
+  imageLoaded=(e)=>{
+    this.setState({
+      showAvatar:e.target.src.indexOf(this.state.prevDisplayName)===-1
+    });
   }
 
   render() {
     const {user_id}=this.props.params;
     return (
       <Content category="Turnusok" title="Felhasználó adatlapja">
-      <div className="row">
-        <div className="col-sm-3">
+        <div className="row">
+          <div className="col-sm-3">
 
-          <div className="text-center">
-              <img src={`https://robohash.org/${this.props.displayName}`}
-                   style={{height:100,width:'auto'}}
-                   className="img-circle "/>
-            <h4> {this.props.displayName}</h4>
+            <div className="text-center">
+
+              <img
+                onLoad={this.imageLoaded}
+                src={`https://robohash.org/${this.props.displayName}`}
+                style={{height:100,width:'auto'}}
+                className={classnames({
+                   "img-circle":true,
+                   "hidden":!this.state.showAvatar
+                   }) }/>
+
+              <h4> {this.props.displayName}</h4>
+            </div>
+
+            <hr/>
+
+            <Nav bsStyle="pills" stacked activeKey={1} onSelect={this.handleSelect}>
+              <LinkContainer to={`/admin/turnmembers/${user_id}/signup-data-1`}>
+                <NavItem eventKey="1.1">Alapinformáció, vállalkozási alapfeltétel</NavItem>
+              </LinkContainer>
+              <LinkContainer to={`/admin/turnmembers/${user_id}/signup-data-2`}>
+                <NavItem eventKey="1.2">Személyes adatok</NavItem>
+              </LinkContainer>
+              <LinkContainer to={`/admin/turnmembers/${user_id}/signup-data-3`}>
+                <NavItem eventKey="1.3">Vállalkozástervezés</NavItem>
+              </LinkContainer>
+              <LinkContainer to={`/admin/turnmembers/${user_id}/signup-agreements`}>
+                <NavItem eventKey="1.4">Nyilatkozat</NavItem>
+              </LinkContainer>
+              <LinkContainer to={`/admin/turnmembers/${user_id}/signup-finalize`}>
+                <NavItem eventKey="1.5">Véglegesítés</NavItem>
+              </LinkContainer>
+            </Nav>
+
           </div>
-
-          <hr/>
-
-          <Nav bsStyle="pills" stacked activeKey={1} onSelect={this.handleSelect}>
-            <LinkContainer to={`/admin/turnmembers/${user_id}/signup-data-1`}>
-              <NavItem eventKey="1.1">Alapinformáció, vállalkozási alapfeltétel</NavItem>
-            </LinkContainer>
-            <LinkContainer to={`/admin/turnmembers/${user_id}/signup-data-2`}>
-              <NavItem eventKey="1.2">Személyes adatok</NavItem>
-            </LinkContainer>
-            <LinkContainer to={`/admin/turnmembers/${user_id}/signup-data-3`}>
-              <NavItem eventKey="1.3">Vállalkozástervezés</NavItem>
-            </LinkContainer>
-            <LinkContainer to={`/admin/turnmembers/${user_id}/signup-agreements`}>
-              <NavItem eventKey="1.4">Nyilatkozat</NavItem>
-            </LinkContainer>
-            <LinkContainer to={`/admin/turnmembers/${user_id}/signup-finalize`}>
-              <NavItem eventKey="1.5">Véglegesítés</NavItem>
-            </LinkContainer>
-          </Nav>
-
+          <div className="col-sm-9">
+            {this.props.children}
+          </div>
         </div>
-        <div className="col-sm-9">
-        {this.props.children}
-        </div>
-      </div>
-        </Content>
+      </Content>
     );
   }
 }
