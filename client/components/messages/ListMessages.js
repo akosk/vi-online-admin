c33f5cde02baea85f33b476194ce782dd04b9936
akosk <akos.kiszely@gmail.com>
2016-12-05 07:04:59 +0100

    resolved #125 Jelentkezési lap: válaszlehetőségek módosítása FIX

--- client/components/messages/ListMessages.js
+++ client/components/messages/ListMessages.js
@@ -1,40 +1,52 @@
+import './style.less';
 import React from 'react';
-import {Popover} from 'react-bootstrap';
 import moment from 'moment';
 
 const ListMessages = ({ messages }) => {
   const rMessages=messages && messages.slice().reverse();
   return (
-    <div>
+    <div clasName="main-wrapper">
       {messages && rMessages.map((message)=> {
         return (
-          <div key={message.created_at}>
-            <div className="row" style={{marginBottom:48}}>
+          <div className="message-container" key={message.created_at}>
 
-              <div className="col-md-1 col-xs-2 text-center">
+              <div className="message-avatar">
+                <div>
                 <img src={`https://robohash.org/${message.from}`} alt="..."
                      className="img-circle img-responsive"
-                    style={{maxHeight:80}}
+                    style={{
+                      width:'50px',
+                      height:'50px'
+                    }}
                 />
-                <span>{message.from}</span>
+
+
+                <p>
+                  {message.from}
+                </p>
+                </div>
               </div>
 
-              <div className="col-md-11 col-xs-10">
-                <Popover
-                  style={{
-                    position:'inherit'
-                  }}
-                  id="popover-basic"
-                  placement="right"
-                  positionLeft={0}
-                  positionTop={0}
-                  title={moment(message.created_at).format('YYYY. MMMM D. HH:ss')}
-                >
-                  {message.message}
-                </Popover>
+            <div className="message-content message-right">
+              <div className="message-arrow"></div>
+              <div style={{marginTop:24}}>
+                {message.message}
               </div>
-
+              <div style={{
+                position:'absolute',
+                right:0,
+                top:0,
+                borderRadius: '6px',
+                textAlign:'right',
+                background:'#efefef',
+                width:'100%',
+                padding:'6px',
+                fontSize:10
+              }}>
+                {moment(message.created_at).format('YYYY. MMMM D. HH:ss')}
+              </div>
             </div>
+
 
           </div>
         );
